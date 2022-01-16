f5779c204b2cd593b64c7c11b09ac2be56fd87cc
Andres Rodriguez <rod16015@byui.edu>
2022-01-16 01:42:00 -0300

    added changes to git

---
+++ src/app/contacts/contact-list/contact.model.ts
@@ -0,0 +1,22 @@
+import { parseHostBindings } from "@angular/compiler";
+
+export class Contact {
+    public id: number;
+    public name: string;
+    public email: string;
+    public phone: string;
+    public imageUrl: string;
+    public group: any;
+
+    constructor(id: number, name: string, email: string, phone: string, imageUrl: string, group: any){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+        this.group = group; 
+    }
+}
+
+
+
